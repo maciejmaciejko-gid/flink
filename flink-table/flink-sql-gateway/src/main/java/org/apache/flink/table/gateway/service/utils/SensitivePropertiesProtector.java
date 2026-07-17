54150d8eea58bf2f9d2af474ca5d3561bec8e791
Maciej Maciejko <maciej.maciejko@xebia.com>
2026-07-17 10:03:55 +0200

    mask sasl config

--- flink-table/flink-sql-gateway/src/main/java/org/apache/flink/table/gateway/service/utils/SensitivePropertiesProtector.java
+++ flink-table/flink-sql-gateway/src/main/java/org/apache/flink/table/gateway/service/utils/SensitivePropertiesProtector.java
@@ -14,10 +14,12 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import static java.util.regex.Pattern.quote;
+
 public class SensitivePropertiesProtector {
 
     private static final String SENSITIVE_OPTIONS =
-            "[^']*" + String.join("[^']*|[^']*", "user", "pass") + "[^']*";
+            "[^']*" + String.join("[^']*|[^']*", "user", "pass", quote("sasl.jaas.config")) + "[^']*";
 
     private static final Pattern CREDENTIALS_PATTERN =
             Pattern.compile(
